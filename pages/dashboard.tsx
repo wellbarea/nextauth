d315528929eae3c04dd91d472cb85052fcb9c8b0
Wellington Barea <wellington.gabriel90@gmail.com>
2022-07-13 08:03:16 -0300

    feat: redirect to server

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -23,8 +23,7 @@ export default function Dashboard(){
 
 export const getServerSideProps = withSSRAuth(async (ctx) => {
     const apiClient = setupAPIClient(ctx)
-    const response = await apiClient.get('/me');
-    console.log(`response`, response)
+    await apiClient.get('/me');
 
     return {
         props: {}
--- utils/withSSRAuth.ts
+++ utils/withSSRAuth.ts
@@ -1,19 +1,34 @@
 import { GetServerSideProps, GetServerSidePropsContext, GetServerSidePropsResult } from "next";
-import { parseCookies } from "nookies";
+import { destroyCookie, parseCookies } from "nookies";
+import { AuthTokenError } from "../services/errors/AuthTokenError";
 
 export function withSSRAuth<T>(fn: GetServerSideProps<T>) {
-    return async (ctx: GetServerSidePropsContext): Promise<GetServerSidePropsResult<T>> => {
-        const cookies = parseCookies(ctx);
+  return async (ctx: GetServerSidePropsContext): Promise<GetServerSidePropsResult<T>> => {
+    const cookies = parseCookies(ctx);
 
-        if (!cookies['nextauth.token']) {
-          return {
-            redirect: {
-              destination: '/',
-              permanent: false,
-            }
+    if (!cookies['nextauth.token']) {
+      return {
+        redirect: {
+          destination: '/',
+          permanent: false,
+        }
+      }
+    }
+
+    try {
+      return await fn(ctx);
+    } catch(err) {
+      if (err instanceof AuthTokenError) {
+        destroyCookie(ctx, 'nextauth.token');
+        destroyCookie(ctx, 'nextauth.refreshToken');
+  
+        return {
+          redirect: {
+            destination: '/',
+            permanent: false
           }
         }
-
-        return await fn(ctx);
+      }
     }
+  }
 }
